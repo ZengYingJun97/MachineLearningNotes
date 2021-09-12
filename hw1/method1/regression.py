190fda4ab19c99062cf516e96f3c6c54e4636c93
ZengYingJun97 <13692916769@163.com>
2021-09-12 20:22:31 +0800

    add

--- hw1/method1/regression.py
+++ hw1/method1/regression.py
@@ -1,5 +1,8 @@
+import csv
+
 import numpy as np
 import pandas as pd
+import math
 
 # 载入数据
 data = pd.read_csv('../data/train.csv', encoding='big5')
@@ -23,3 +26,53 @@ for month in range(12):
                 continue
             x[month * 471 + day * 24 + hour, :] = month_data[month][:, day * 24 + hour: day * 24 + hour + 9].reshape(1, -1)
             y[month * 471 + day * 24 + hour, 0] = month_data[month][9, day * 24 + hour + 9]
+
+#标准化
+mean_x = np.mean(x, axis=0)
+std_x = np.std(x, axis=0)
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        if std_x[j] != 0:
+            x[i][j] = (x[i][j] - mean_x[j]) / std_x[j]
+
+#训练
+size = 18 * 9 + 1
+w_set = np.zeros([size, 1])
+x_set = np.concatenate((np.ones([12 * 471, 1]), x), axis=1).astype(float)
+learning_rate = 100
+iter_time = 1000
+adagrad = np.zeros([size, 1])
+eps = 0.000000001
+for t in range(iter_time):
+    pre_y = np.dot(x_set, w_set)
+    loss = np.sqrt(np.sum(np.power(pre_y - y, 2)) / (471 * 12))
+    gradient = 2 * np.dot(x_set.transpose(), pre_y - y)
+    adagrad += gradient ** 2
+    w_set = w_set - learning_rate * gradient / np.sqrt(adagrad + eps)
+np.save('weight.npy', w_set)
+
+#测试
+testData = pd.read_csv('../data/test.csv', header=None, encoding='big5')
+test_data = testData.iloc[:, 2:]
+test_data[test_data == 'NR'] = 0
+test_data = test_data.to_numpy()
+test_x = np.empty([240, 18 * 9], dtype=float)
+for i in range(240):
+    test_x[i, :] = test_data[18 * i: 18 * (i + 1), :].reshape(1, -1)
+for i in range(len(test_x)):
+    for j in range(len(test_x[0])):
+        if std_x[j] != 0:
+            test_x[i][j] = (test_x[i][j] - mean_x[j]) / std_x[j]
+test_x = np.concatenate((np.ones([240, 1]), test_x), axis=1).astype(float)
+test_x
+w_set = np.load('weight.npy')
+ans_y = np.dot(test_x, w_set)
+
+#保存数据
+with open('submit.csv', mode='w', newline='') as submit_file:
+    csv_writer = csv.writer(submit_file)
+    header = ['id, value']
+    csv_writer.writerow(header)
+    for i in range(240):
+        row = ['id_' + str(i), ans_y[i][0]]
+        csv_writer.writerow(row)
