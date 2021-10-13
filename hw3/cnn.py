974263f8ec364c99c365fce8febd281bcb426bec
ZengYingJun97 <13692916769@163.com>
2021-10-13 15:43:44 +0800

    add cnn.py

---
+++ hw3/cnn.py
@@ -0,0 +1,181 @@
+import os
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+import time
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def readFile(path, label):
+    image_dir = sorted(os.listdir(path))
+    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    y = np.zeros(len(image_dir), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))
+        x[i, :, :] = cv2.resize(img, (128, 128))
+        if label:
+            y[i] = int(file.split("_")[0])
+    if label:
+        return x, y
+    else:
+        return x
+data_dir = './data'
+train_x, train_y = readFile(os.path.join(data_dir, 'training'), True)
+validation_x, validation_y = readFile(os.path.join(data_dir, 'validation'), True)
+test_x = readFile(os.path.join(data_dir, 'testing'), False)
+
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+])
+
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor(),
+])
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.x = x
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]
+            return X, Y
+        else:
+            return X
+
+batch_size = 128
+train_set = ImgDataset(train_x, train_y, train_transform)
+validation_set = ImgDataset(validation_x, validation_y, test_transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+validation_loader  = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 11)
+        )
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
+
+model = Classifier().to(device)
+loss = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+num_epoch = 30
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+    validation_acc = 0.0
+    validation_loss = 0.0
+    model.train()
+    for i, data in enumerate(train_loader):
+        optimizer.zero_grad()
+        train_pred = model(data[0].to(device))
+        batch_loss = loss(train_pred, data[1].to(device))
+        batch_loss.backward()
+        optimizer.step()
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+
+    model.eval()
+    with torch.no_grad():
+        for i, data in enumerate(validation_loader):
+            validation_pred = model(data[0].to(device))
+            batch_loss = loss(validation_pred, data[1].to(device))
+
+            validation_acc += np.sum(np.argmax(validation_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+            validation_loss += batch_loss.item()
+
+train_val_x = np.concatenate((train_x, validation_x), axis=0)
+train_val_y = np.concatenate((train_y, validation_y), axis=0)
+train_val_set = ImgDataset(train_val_x, train_val_y, train_transform)
+train_val_loader = DataLoader(train_val_set, batch_size=batch_size, shuffle=True)
+
+model_best = Classifier().to(device)
+loss = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model_best.parameters(), lr=0.001)
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+
+    model_best.train()
+    for i, data in enumerate(train_val_loader):
+        optimizer.zero_grad()
+        train_pred = model_best(data[0].to(device))
+        batch_loss = loss(train_pred, data[1].to(device))
+        batch_loss.backward()
+        optimizer.step()
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+
+test_set = ImgDataset(test_x, transforms=test_transform)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+model_best.eval()
+prediction = []
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        test_pred = model_best(data.to(device))
+        test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
+        for y in test_label:
+            prediction.append(y)
+
+with open("predict.csv", 'w') as f:
+    f.write('Id,Category\n')
+    for i, y in enumerate(prediction):
+        f.write('{},{}\n'.format(i, y))
+
